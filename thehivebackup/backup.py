ffc06252112a9d08be946606a917ca9fc087e3e8
Jonas Plum <cugu@users.noreply.github.com>
2021-04-19 21:48:21 +0200

    Rewrite backup (#3)
    * Rewrite backup
    
    * Fix pylint errors

--- thehivebackup/backup.py
+++ thehivebackup/backup.py
@@ -1,88 +1,59 @@
 import datetime
-import http.client
 import json
-import logging
 import os
-import ssl
+
 from multiprocessing import Pool
+
+from thehive4py.api import TheHiveApi
+from thehive4py.query import Between
 
 
 class Backupper:
 
-    def __init__(self, backupdir: str, host: str, api_key: str,
-                 org: str = "", port: int = 80, use_ssl: bool = True, verify: bool = True):
-        self.host = host
-        self.port = port
-        self.ssl = use_ssl
-        self.verify = verify
-        self.api_key = api_key
+    def __init__(self, backupdir: str, url: str, api_key: str, org: str = None, verify: bool = True):
+        url = url.strip("/")
+        self.api = TheHiveApi(url, api_key, organisation=org)
 
-        self.org = org
+        if not verify:
+            self.api.cert = False
 
         self.backupdir = f'{backupdir}-{int(datetime.datetime.utcnow().timestamp())}'
         os.makedirs(self.backupdir, exist_ok=True)
         self.case_file = os.path.join(self.backupdir, 'cases.jsonl')
         self.alert_file = os.path.join(self.backupdir, 'alerts.jsonl')
 
-    def _conn(self):
-        if self.ssl:
-            if self.verify:
-                return http.client.HTTPSConnection(self.host, self.port)
-            return http.client.HTTPSConnection(self.host, self.port, context=ssl._create_unverified_context())
-        return http.client.HTTPConnection(self.host, self.port)
-
-    def request(self, method: str, url: str, api_key: str, data: dict = None) -> bytes:
-        conn = self._conn()
-        headers = {'Authorization': 'Bearer ' + api_key}
-        if self.org != "":
-            headers['X-Organisation'] = self.org
-        if data is not None:
-            headers['Content-type'] = 'application/json'
-            data = json.dumps(data)
-        conn.request(method, url, data, headers=headers)
-        response = conn.getresponse()
-        resp = response.read()
-        if response.status != 200 and response.status != 201:
-            logging.warning(url)
-            logging.warning(resp)
-        return resp
-
-    def get_file(self, file_id: str, api_key: str):
+    def get_file(self, attachment_id: str):
         os.makedirs(os.path.join(self.backupdir, 'attachments'), exist_ok=True)
-        response = self.request('GET', f'/api/datastore/{file_id}', api_key)
-        with open(os.path.join(self.backupdir, 'attachments', file_id), 'wb') as io:
+        response = self.api.download_attachment(attachment_id)
+        with open(os.path.join(self.backupdir, 'attachments', attachment_id), 'wb') as io:
             io.write(response)
 
     def backup_cases_all(self) -> [dict]:
-        cases = self.request('GET', '/api/case?range=all', self.api_key)
-        self._backup_cases(json.loads(cases))
+        cases = self.api.find_cases(query={}, sort=['-createdAt'], range='all').json()
+        self._backup_cases(cases)
 
     def backup_cases_range(self, start, end) -> [dict]:
-        query = {'query': {'_between': {'_field': 'createdAt', '_from': start, '_to': end}}}
-        cases = self.request('POST', '/api/case/_search?range=all', self.api_key, query)
-        self._backup_cases(json.loads(cases))
+        query = Between("createdAt", start, end)
+        cases = self.api.find_cases(query=query, sort=['-createdAt'], range='all').json()
+        self._backup_cases(cases)
 
-    def _backup_cases(self, cases):
+    def _backup_cases(self, cases: [dict]):
         with open(self.case_file, 'w+', encoding='utf8') as io:
             pool = Pool(processes=8)
             for case in cases:
                 json.dump(case, io)
                 io.write('\n')
-                # self.backup_observables(case['id'])
-                # self.backup_tasks(case['id'])
 
             pool.map(self._backup_case, cases)
             pool.close()
             pool.join()
 
-    def _backup_case(self, case):
+    def _backup_case(self, case: dict):
         self.backup_observables(case['id'])
         self.backup_tasks(case['id'])
 
     def backup_tasks(self, case_id: str) -> [dict]:
-        query = {'query': {'_parent': {'_type': 'case', '_query': {'_id': case_id}}}}
-        tasks = self.request('POST', '/api/case/task/_search?range=all', self.api_key, query)
-        tasks = json.loads(tasks)
+        tasks = self.api.get_case_tasks(case_id=case_id).json()
         if tasks:
             case_path = os.path.join(self.backupdir, 'cases', case_id)
             os.makedirs(case_path, exist_ok=True)
@@ -93,8 +64,7 @@ class Backupper:
                     self.backup_logs(case_id, task['id'])
 
     def backup_logs(self, case_id: str, task_id: str) -> [dict]:
-        logs = self.request('GET', f'/api/case/task/{task_id}/log', self.api_key)
-        logs = json.loads(logs)
+        logs = self.api.get_task_logs(task_id).json()
         if logs:
             task_path = os.path.join(self.backupdir, 'cases', case_id, 'tasks', task_id)
             os.makedirs(task_path, exist_ok=True)
@@ -103,12 +73,10 @@ class Backupper:
                     json.dump(log, io)
                     io.write('\n')
                     if 'attachment' in log:
-                        self.get_file(log['attachment']['id'], self.api_key)
+                        self.get_file(log['attachment']['id'])
 
     def backup_observables(self, case_id: str):
-        query = {'query': {'_parent': {'_type': 'case', '_query': {'_id': case_id}}}}
-        observables = self.request('POST', '/api/case/artifact/_search?range=all', self.api_key, query)
-        observables = json.loads(observables)
+        observables = self.api.get_case_observables(case_id).json()
         if observables:
             os.makedirs(os.path.join(self.backupdir, 'cases', case_id), exist_ok=True)
             with open(os.path.join(self.backupdir, 'cases', case_id, 'observables.jsonl'), 'w+', encoding='utf8') as io:
@@ -116,17 +84,15 @@ class Backupper:
                     json.dump(observable, io)
                     io.write('\n')
                     if 'attachment' in observable:
-                        self.get_file(observable['attachment']['id'], self.api_key)
+                        self.get_file(observable['attachment']['id'])
 
     def backup_alerts_all(self):
-        alerts = self.request('GET', '/api/alert?range=all', self.api_key)
-        alerts = json.loads(alerts)
+        alerts = self.api.find_alerts(query={}, sort=['-createdAt'], range='all').json()
         self._backup_alerts(alerts)
 
     def backup_alerts_range(self, start, end):
-        query = {'query': {'_between': {'_field': 'createdAt', '_from': start, '_to': end}}}
-        alerts = self.request('POST', '/api/alert/_search?range=all', self.api_key, query)
-        alerts = json.loads(alerts)
+        query = Between("createdAt", start, end)
+        alerts = self.api.find_alerts(query=query, sort=['-createdAt'], range='all').json()
         self._backup_alerts(alerts)
 
     def _backup_alerts(self, alerts):
--- thehivebackup/main.py
+++ thehivebackup/main.py
@@ -14,6 +14,8 @@ from thehivebackup.recover import Restorer
 
 logger = logging.getLogger(__name__)
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 
 def main():
     parser = argparse.ArgumentParser(prog="thehivebackup", description="Backup and restore TheHive remotely.")
@@ -123,9 +125,8 @@ def utc_nano_timestamp(year: int, month: int, day: int) -> int:
 
 def backup(args):
     start_time = time.time()
-    ssl, host, port = parse_url(args.host)
     if args.year is None and args.month is None:
-        migration = Backupper(f"backup-{args.org}-full", host, args.key, args.org, port, ssl, args.verify)
+        migration = Backupper(f"backup-{args.org}-full", args.host, args.key, args.org, args.verify)
         migration.backup_cases_all()
         migration.backup_alerts_all()
     elif args.year is not None and args.month is not None:
@@ -140,7 +141,7 @@ def backup(args):
             name = f"backup-{args.org}-{args.year}-{args.month}-{args.day}"
             start = utc_nano_timestamp(args.year, args.month, args.day)
             end = start + 60 * 60 * 24 * 1000 - 1
-        migration = Backupper(name, host, args.org, args.key, port, ssl, args.verify)
+        migration = Backupper(name, args.host, args.org, args.key, args.verify)
         migration.backup_cases_range(start, end)
         migration.backup_alerts_range(start, end)
     else:
